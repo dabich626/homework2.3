bf3ec139065902ca27c78eaeb80b3365a6a314cc
dabi <dabich626626@gmail.com>
2023-06-26 14:36:55 +0200

    /

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car extends Bicycle {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+
+}}
